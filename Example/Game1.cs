791b915409a1d89c4178bcf08efe3754655c6193
sascha-schmidt217 <sascha.schmidt217@gmail.com>
2018-11-19 12:55:38 +0100

    ...

--- Example/Game1.cs
+++ Example/Game1.cs
@@ -12,6 +12,7 @@ namespace Example
 
         private Image _floor;
         private Image _logo;
+        private Image _logo2;
         private float _viewRot;
         private float _lightRot;
 
@@ -27,7 +28,9 @@ namespace Example
 
             _floor = new Image("Images/tiles");
             _logo = new Image("Images/logo");
+            _logo2 = new Image("Images/logo2");
             _logo.ShadowCaster = new ShadowCaster(_logo.Width / 2, 24);
+            _logo2.ShadowCaster = new ShadowCaster(new Rectangle(-_logo2.Width/2, -_logo2.Height/2, _logo2.Width, _logo2.Height));
         }
 
         protected override void Update(GameTime gameTime)
@@ -68,11 +71,18 @@ namespace Example
             }
 
             // Draw sprites
+            int k = 0;
             for (float an = 0; an < System.Math.PI * 2; an += (float)System.Math.PI * 2 / 8)
             {
+               
                 float xx = Width / 2 + (float)System.Math.Cos(an) * Width / 4;
                 float yy = Height / 2 + (float)System.Math.Sin(an) * Height / 4;
-                Canvas.DrawImage(_logo, xx, yy);
+                if (k % 2 == 0)
+                    Canvas.DrawImage(_logo, xx, yy);
+                else
+                    Canvas.DrawImage(_logo2, xx, yy);
+
+                k++;
             }
 
             var mat = Canvas.Matrix;
@@ -83,7 +93,18 @@ namespace Example
             for(int i = 0; i < 4; ++i)
             {
                 Canvas.Color = new Color(rnd.Next(128, 255), rnd.Next(128, 255), rnd.Next(128, 255));
-                Canvas.AddSpotLight(mouse.X, mouse.Y, _lightRot * RAD_TO_DEG + i * 90, 512, 15, 35, 4, 16);
+
+                if (i % 2 == 0)
+                {
+                    Canvas.AddSpotLight(mouse.X, mouse.Y, _lightRot * RAD_TO_DEG + i * 90, 512, 15, 35, 4, 16);
+                }
+                else
+                {
+                    float angle = _lightRot + i * (float)MathHelper.Pi / 2;
+                    Canvas.AddPointLight(
+                        mouse.X + (float)System.Math.Cos(angle) * 64,
+                        mouse.Y - (float)System.Math.Sin(angle) * 64, 300, 4, 8);
+                }
             }
 
             Canvas.SetMatrix(mat);
