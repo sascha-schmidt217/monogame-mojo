7df70553fbb3ff38daa4aaab5ffac2f6ab4de69e
sascha-schmidt217 <sascha.schmidt217@gmail.com>
2018-11-19 10:44:16 +0100

    added example

---
+++ Example/Content/Effects/spot_light.fx
@@ -0,0 +1,79 @@
+﻿#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+//float intensity = clamp(1.0f - (dist / 32.0f), 0.0f, 1.0f);
+
+//float minLight = 0.1f;
+//float radius = 32.0f;
+//float a = 0;
+//float b = 1.0 / (radius*radius * minLight);
+//float intensity = 1.0f / (1.0f + b *dist*dist);
+
+//float intensity = clamp(1.0f - dist / radius, 0.0f, 1.0f); 
+//intensity *= intensity;
+
+texture shadowMap: register(t0);
+sampler shadowMapSampler: register(s0);
+
+
+matrix WorldViewProj;
+float2 lightPos;
+float2 lightDir;
+float intensity;
+float radius;
+float inner;
+float outer;
+float2 inv_tex_size;
+
+struct VertexShaderInput
+{
+	float4 Position : POSITION0;
+	float4 Color	: COLOR0;
+	float2 TexCoord : TEXCOORD;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position		: SV_POSITION;
+	float4 Color		: COLOR0;
+	float2 TexCoord		: TEXCOORD0;
+};
+
+VertexShaderOutput MainVS(VertexShaderInput input)
+{
+	VertexShaderOutput output = (VertexShaderOutput)0;
+	output.Position = mul(input.Position, WorldViewProj);
+	output.Color = input.Color;
+	output.TexCoord = input.Position.xy;
+	return output;
+}
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+	// cutoff angle factor
+	float2 dir = input.TexCoord - lightPos;
+	float angle = acos(dot(normalize(lightDir), normalize(dir)));
+	float f = 1.0f - smoothstep(inner, outer, angle);
+
+	// lighting attentuation
+	float dist = length(dir);
+	float alpha = pow(clamp(1.0f - dist / radius, 0.0f, 1.0f), intensity);
+
+	// shadow
+	float shadow =  tex2D(shadowMapSampler, input.TexCoord * inv_tex_size).a;
+
+	return float4(input.Color.r, input.Color.g, input.Color.b, alpha*f*shadow);
+}
+
+technique BasicColorDrawing {
+	pass P0 {
+		VertexShader = compile VS_SHADERMODEL MainVS();
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+}
---
+++ Example/Game1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using Mojo.Graphics;
+
+namespace Example
+{
+    /*
+    
+        Hi, 
+        I'd like to share this with you, maybe someone else will find it useful.
+        Since monogame doesn't have an easy to use high-level 2d API, except SpriteBAtch, 
+        I more or less ported Monkey2's 2D Drawing API 
+        
+        - no 1:1 port
+        - 2d-NormalMapping is not yet implemented
+
+    */
+
+    public class Game1 : MojoGame
+    {
+        private const float RAD_TO_DEG = 57.2957795130823208767981548141052f;
+
+        private Image _floor;
+        private Image _logo;
+        private float _viewRot;
+        private float _lightRot;
+
+        public Game1() : base(640,480,false)
+        {
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            base.LightingEnabled = true;
+
+            _floor = new Image("Images/tiles");
+            _logo = new Image("Images/logo");
+            _logo.ShadowCaster = new ShadowCaster(_logo.Width / 2, 24);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            var mouse = Mouse.GetState();
+          
+            _viewRot += 0.001f;
+            _lightRot += 0.02f;
+
+            // Set Ambient color used for lighting
+            Canvas.TextureFilteringEnabled = true;
+            Canvas.AmbientColor = new Color(32, 32, 32);
+            Canvas.Clear(Color.Black);
+
+            Canvas.Translate(Width / 2, Height / 2);
+            Canvas.Scale((float)System.Math.Sin(_viewRot * 3) * 0.25f + 1.25f, (float)System.Math.Cos(_viewRot * 5) * 0.25f + 1.25f);
+            Canvas.Rotate(_viewRot* RAD_TO_DEG);
+            Canvas.Translate(-Width / 2, -Height / 2);
+
+            // Draw background
+            Canvas.Color = Color.White;
+            for (int x = 0; x < Width; x += _floor.Width)
+            {
+                for (int y = 0; y < Height; y += _floor.Height)
+                {
+                    Canvas.DrawImage(_floor, x, y);
+                }
+            }
+
+            // Draw sprites
+            for (float an = 0; an < System.Math.PI * 2; an += (float)System.Math.PI * 2 / 8)
+            {
+                float xx = Width / 2 + (float)System.Math.Cos(an) * Width / 4;
+                float yy = Height / 2 + (float)System.Math.Sin(an) * Height / 4;
+                Canvas.DrawImage(_logo, xx, yy);
+            }
+
+            var mat = Canvas.Matrix;
+            Canvas.ResetMatrix();
+
+            // add lights
+            var rnd = new System.Random(0);
+            for(int i = 0; i < 4; ++i)
+            {
+                Canvas.Color = new Color(rnd.Next(128, 255), rnd.Next(128, 255), rnd.Next(128, 255));
+                Canvas.AddSpotLight(mouse.X, mouse.Y, _lightRot * RAD_TO_DEG + i * 90, 512, 15, 35, 4, 16);
+            }
+
+            Canvas.SetMatrix(mat);
+
+            base.Draw(gameTime);
+        }
+    }
+}
